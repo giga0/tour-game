1c5eddfdddad9a5e2b487adc387666df72e621ae
Igor Bogdanovic <igor.s.bogdanovic@gmail.com>
2020-07-04 17:38:39 +0200

    yarn upgrade

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-if (process.env.NODE_ENV === 'development') {
-  Vue.config.productionTip = process.env.VUE_APP_PRODUCTION_TIP === 'true'
-}
+Vue.config.productionTip = process.env.VUE_APP_PRODUCTION_TIP
 
 new Vue({
   render: h => h(App),
